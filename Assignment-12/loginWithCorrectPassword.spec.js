62309a4f720d4fbacf35ab3ee0f091f609ac3257
praveen <praveen.ponduru@pal.tech>
2024-01-24 14:39:20 +0530

    Assignment 12 changes

---
+++ Assignment-12/loginWithCorrectPassword.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://automationexercise.com/');
+
+  await page.getByRole('link', { name: ' Signup / Login' }).click();
+  await expect(page.locator('#form')).toContainText('Login to your account');
+
+  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill('praveen.ponduru@test.com');
+  await page.getByPlaceholder('Password').fill('Sh3rl@ck');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: ' Delete Account' }).click();
+  await expect(page.locator('b')).toContainText('Account Deleted!');
+});
---
+++ Assignment-15/cart.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test.only('Add products to carts', async ({ page }) => {
+
+  await page.goto('https://automationexercise.com/');
+  await page.getByRole('link', { name: ' Products' }).click();
+  await page.locator('.overlay-content > .btn').first().click({ force: true });
+  await page.getByRole('button', { name: 'Continue Shopping' }).click({ force: true });
+  await page.locator("locator('.productinfo > .btn').first()").hover();
+  await page.locator('div:nth-child(4) > .product-image-wrapper > .single-products > .product-overlay > .overlay-content > .btn').click({ force: true });
+  await page.getByRole('button', { name: 'Continue Shopping' }).click({ force: true });
+  await page.getByRole('link', { name: ' Cart' }).click();
+  await page.getByText('Rs.').nth(1).click();
+});
